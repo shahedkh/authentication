03c1c646084ee1496e8eee6541d6e969d824d71d
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-18 02:45:17 +0300

    Protecting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
-
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import { Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
   return (
     <Layout>
       <Routes>
-        <Route path='/' element={<HomePage />}/>
-        <Route path='/auth' element={<AuthPage />}/>
-        <Route path='/profile' element={  <UserProfile />}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={!authCtx.isLoggedIn && <AuthPage />} />
+        <Route
+          path="/profile"
+          element={authCtx.isLoggedIn && <UserProfile />}
+        />
       </Routes>
     </Layout>
   );
